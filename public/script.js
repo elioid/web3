ed700a7da40c6cc95f66fd429c63c7a1d840ae78
elote <elote365cpp@gmail.com>
2026-03-03 23:16:47 -0600

    RKWell's first mockup

--- public/script.js
+++ public/script.js
@@ -1,16 +1,18 @@
 const label = document.getElementById("counterValue");
 const button = document.getElementById("buttonCounter");
 
+// función para poner counter con num actual
 async function cargarCounter() {
-    const response = await fetch("/getcounter");
-    const data = await response.json();
-    label.textContent = data.value;
+    const response = await fetch("/getcounter"); //llamar servidor
+    const data = await response.json(); // obtener respuesta (valor actual)
+    label.textContent = data.value; //poner en etiqueta
 }
 
+// función para aumentar cickeando botón
 button.addEventListener("click", async()=>{
-    const response = await fetch("/increment", { method: "POST" });
-    const data = await response.json();
-    label.textContent = data.newVal;
+    const response = await fetch("/increment", { method: "POST" }); //hacer post con metodo
+    const data = await response.json(); //obtener respuesta del newValue de json
+    label.textContent = data.newVal; // poner en etiqueta
 });
 
 cargarCounter();
